f8f23c513d033d6094c9abd69d632e447c0a91f3
kristijanw <freelance.kristijan@gmail.com>
2023-05-24 23:25:46 +0200

    api link

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 // https://staging.aether.hr/backend/api
 // http://192.168.1.5:8000/api
-const baseURL = 'http://192.168.1.5:8000/api';
+const baseURL = 'https://staging.aether.hr/backend/api';
 const loginURL = '$baseURL/login';
 const registerURL = '$baseURL/register';
 const registerNewURL = '$baseURL/register-new';
