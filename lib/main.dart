6b8cdc22db33d5fa50919f056b91e905447097e5
1 <a1@1s-MacBook-Pro.local>
2023-11-02 11:14:40 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,12 @@
-import 'dart:developer';
-
-import 'package:app/firebase_options.dart';
 import 'package:app/screens/loading.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:app/services/local_notif.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-import 'services/local_notif.dart';
-
-FlutterLocalNotificationsPlugin locNotf = FlutterLocalNotificationsPlugin();
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  LocalNotification.init(locNotf);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  requestPermission();
-  listenMessage();
-
-  FirebaseMessaging.instance.getToken().then((value) {
-    log("$value");
-  });
+  FCMNotification.init();
 
   initializeDateFormatting().then((_) => runApp(const App()));
 }
@@ -49,41 +25,4 @@ class App extends StatelessWidget {
       home: Loading(),
     );
   }
-}
-
-void requestPermission() async {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: false,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-
-  // ignore: avoid_print
-  print('User granted permission: ${settings.authorizationStatus}');
-}
-
-void listenMessage() async {
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    if (message.notification != null) {
-      // ignore: avoid_print
-      print('Message also contained a notification: ${message.notification}');
-
-      LocalNotification.showNotification(
-        title: message.notification!.title ?? 'Title',
-        body: message.notification!.body ?? 'Body',
-        flutterLocalNotificationsPlugin: locNotf,
-      );
-    }
-
-    if (message.notification != null) {
-      // ignore: avoid_print
-      print('Message also contained a notification: ${message.notification}');
-    }
-  });
 }
--- lib/services/local_notif.dart
+++ lib/services/local_notif.dart
@@ -1,22 +1,118 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:developer';
 
-class LocalNotification {
-  static Future init(FlutterLocalNotificationsPlugin localNotifPlugin) async {
+import 'package:app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:developer' as developer;
+
+FlutterLocalNotificationsPlugin _localNotifPlugin =
+    FlutterLocalNotificationsPlugin();
+
+class FCMNotification {
+  static Future<void> init() async {
     var andInit = const AndroidInitializationSettings('@mipmap/ic_launcher');
     DarwinInitializationSettings initSettingsDarwin =
         DarwinInitializationSettings(
-            requestAlertPermission: true,
-            requestBadgePermission: true,
-            requestSoundPermission: true,
-            onDidReceiveLocalNotification: (int id, String? title, String? body,
-                String? payload) async {});
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification:
+          (int id, String? title, String? body, String? payload) async {},
+    );
 
     var initSettings = InitializationSettings(
       android: andInit,
       iOS: initSettingsDarwin,
     );
 
-    await localNotifPlugin.initialize(initSettings);
+    await _localNotifPlugin.initialize(initSettings);
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    //await localNotifPlugin.initialize(initSettings);
+
+    requestPermission();
+    getToken();
+
+    FirebaseMessaging.onBackgroundMessage((message) async {
+      log(message.toString());
+    });
+    FirebaseMessaging.onMessage.listen((message) async {
+      log(message.toString());
+    });
+
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      log(initialMessage.toString());
+    }
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) async {
+      log(message.toString());
+    });
+
+    listenMessage();
+  }
+
+  static requestPermission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    developer.log('User granted permission: ${settings.authorizationStatus}');
+  }
+
+  static Future<String> getToken() async {
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    developer.log('Moj fcm token: $fcmToken');
+    return fcmToken.toString();
+  }
+
+  static listenMessage() async {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      log("$message");
+
+      if (message.notification != null) {
+        developer.log(
+          'Message also contained a notification: ${message.notification}',
+        );
+
+        RemoteNotification? notification = message.notification;
+        showNotification(
+          title: notification!.title ?? '',
+          body: notification.body ?? '',
+          flutterLocalNotificationsPlugin: _localNotifPlugin,
+        );
+      }
+
+      if (message.notification != null) {
+        developer.log(
+          'Message also contained a notification: ${message.notification}',
+        );
+      }
+    });
   }
 
   static Future showNotification({
